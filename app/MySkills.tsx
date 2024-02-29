283ed98727fe208d907ce9dd28b0e35fa8d5b56f
DRH <dragonfourtyseven@gmail.com>
2024-02-29 17:51:14 +0500

    feat: adding new projects

--- app/MySkills.tsx
+++ app/MySkills.tsx
@@ -11,7 +11,7 @@ const MySkills = ({data}: { data: HeroSection }) => {
         <div
             className="p-5 border-t bg-background sm:p-10 w-[100vw] relative left-[50%] right-[50%] ml-[-50vw] mr-[-50vw]">
           <Container>
-            <div className="flex justify-end items-end ">
+            <div className="flex justify-end items-end">
               <div className="flex flex-col gap-3">
                 <div className="font-semibold sm:text-3xl text-2xl">
                   My Skills
