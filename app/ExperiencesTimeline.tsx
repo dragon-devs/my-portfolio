774032565b78999a371b9315db95a062689d1308
DRH <dragonfourtyseven@gmail.com>
2024-03-05 03:45:14 +0500

    fix: fix bugs and updated

--- app/ExperiencesTimeline.tsx
+++ app/ExperiencesTimeline.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import experience from "../change_data/experience.json"
+import {Badge} from "@/components/ui/badge";
 
 const ExperiencesTimeline = () => {
     return (
-        <div className="sm:py-10 py-5 px-5 flex flex-col">
+        <div className="sm:py-10 py-5 px-3 flex flex-col">
             <h1 className="font-semibold sm:text-3xl flex sm:mb-10 mb-5 justify-center text-2xl">
                 My Experience
             </h1>
             {experience.map(item => (
-                <div className="relative flex sm:grid grid-cols-3">
-                    <div className="grid justify-end absolute sm:relative bottom-0 right-0 sm:mr-6 font-semibold mt-1 ">
+                <div key={item.title} className="relative flex sm:grid grid-cols-3">
+                    <div className="grid justify-end absolute sm:relative bottom-2 right-0 sm:mr-6 font-semibold mt-1 ">
                         {item.from} - {item.to}
                     </div>
                     <ol className="relative border-s-2 col-span-2 border-primary dark:border-primary">
@@ -25,10 +26,15 @@ const ExperiencesTimeline = () => {
                             d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                     </svg>
                     </span>
-                        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                        <h3 className="flex items-center capitalize mb-1 text-sm sm:text-lg font-semibold text-gray-900 dark:text-white">
                             {item.title}
+                            <Badge
+                                className="ms-3 uppercase">
+
+                                {item.company}
+                            </Badge>
                         </h3>
-                        <p className="text-base font-normal text-muted-foreground  ">
+                        <p className="text-sm sm:text-base font-normal text-muted-foreground  ">
                             {item.description}
                         </p>
                         </li>
