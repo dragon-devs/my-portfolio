1c2014552d0fa0975102265dbde6ccc2647d2977
DRH <dragonfourtyseven@gmail.com>
2024-03-06 01:40:37 +0500

    fixed: updated hero image

--- app/HeroSection.tsx
+++ app/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection = ({data}: Props) => {
   return (
       <div className="relative flex flex-col-reverse">
         <div className="flex flex-col gap-3 rounded-md bg-background sm:p-0 p-5 sm:mt-0 mt-52 pt-3 sm:mb-0 sm:pt-0 sm:pb-0 sm:bg-transparent">
-          <h1 className="font-bold text-lg sm:text-2xl ">Hello👋, I&apos;m {my_name}</h1>
+          <h1 className="font-bold text-lg sm:text-2xl sm:mt-0 mt-2">Hello👋, I&apos;m {my_name}</h1>
           <h1 className="font-black text-4xl sm:text-6xl text-primary/80 dark:text-primary/80 sm:dark:text-primary/60 md:w-[50%]">{job_title}</h1>
           <h2 className="font-medium text-2xl sm:text-4xl">{title_description}</h2>
           <div className="flex sm:gap-3 gap-2 capitalize">
