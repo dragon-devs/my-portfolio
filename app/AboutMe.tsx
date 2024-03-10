f14c23fb293096b3ed869e608933b8727bb15f84
DRH <dragonfourtyseven@gmail.com>
2024-03-10 23:45:51 +0500

    feat: new project picture added

--- app/AboutMe.tsx
+++ app/AboutMe.tsx
@@ -3,26 +3,29 @@ import Container from "@/components/Container";
 import me from "@/change_data/hero_section.json";
 
 const AboutMe = () => {
-  return (
-      <div id="about">
-        <div className="p-5 border-t bg-background sm:p-10 w-[100vw] relative left-[50%] right-[50%] ml-[-50vw] mr-[-50vw]">
-          <Container>
-            <div className="flex flex-col gap-2 sm:gap-4">
-              <h1 className="font-semibold sm:text-3xl text-2xl">
-                About Me
-              </h1>
-              <p className="text-muted-foreground sm:w-[47%] sm:leading-8">
-                {me.about_me}
-              </p>
-                <div className="flex-wrap flex gap-2">
-                    <p>&quot;Simplicity is the key of brilliance&quot;</p><p className="font-semibold"> - Bruce lee</p>
-                </div>
-            </div>
-          </Container>
-        </div>
-      </div>
+    return (
+        <div id="about">
+            <div
+                className="p-5 border-t bg-background sm:p-10 w-[100vw] relative left-[50%] right-[50%] ml-[-50vw] mr-[-50vw]">
+                <Container>
+                    <div className="flex flex-col gap-2 sm:gap-4">
+                        <h1 className="font-semibold sm:text-3xl text-2xl">
+                            About Me
+                        </h1>
+                        <p className="text-muted-foreground sm:w-[47%] sm:leading-8">
+                            {me.about_me}
+                        </p>
+                        <div className="flex-wrap flex gap-2">
 
-  );
+                            <p>&quot;Simplicity is the key of brilliance&quot;</p><p className="font-semibold"> - Bruce
+                            lee</p>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </div>
+
+    );
 };
 
 export default AboutMe;
