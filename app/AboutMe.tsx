90dabb76ae9d36b10654c830711e4c8f506efb8f
DRH <dragonfourtyseven@gmail.com>
2024-06-09 23:51:02 +0530

    quoted by author

--- app/AboutMe.tsx
+++ app/AboutMe.tsx
@@ -16,8 +16,7 @@ const AboutMe = () => {
                             {me.about_me}
                         </p>
                         <div className="flex-wrap flex gap-2 animate-fade-left">
-                            <p>&quot;Simplicity is the key to brilliance&quot;</p><p className="font-semibold"> - Bruce
-                            lee</p>
+                            <p>&quot;{me.quote}&quot;</p><p className="font-semibold"> - {me.author}</p>
                         </div>
                     </div>
                 </Container>
